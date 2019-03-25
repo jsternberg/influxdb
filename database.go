e3f0ab4509164d8ff3f0edf867efb29f2bfbbd55
Jonathan A. Sternberg <jonathan@influxdata.com>
2019-03-25 15:08:45 -0500

    Initial commit

---
+++ database.go
@@ -0,0 +1,8 @@
+package influxdb
+
+import "context"
+
+// Run the super awesome version 2 of the database.
+func Run(ctx context.Context) error {
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jsternberg/influxdb/v2
+
+go 1.12
